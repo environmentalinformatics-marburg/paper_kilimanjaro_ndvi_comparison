776a65fb4f32acac4d469717b5337cb03db25773
fdetsch <florian.detsch@staff.uni-marburg.de>
2015-08-05 17:25:12 +0200

    Enabled user-controlled function definition for spatial aggregation; finalized spatial aggregation when two datasets of different spatial resolution are supplied; adjusted figure.

--- R/fig02_ioa.R
+++ R/fig02_ioa.R
@@ -45,28 +45,15 @@ ls_ndvi <- lapply(c("mod13q1", "myd13q1", "gimms"), function(i) {
 
 p_ioa <- foreach(i = list(ls_ndvi[[1]], ls_ndvi[[1]], ls_ndvi[[2]]), 
                  j = list(ls_ndvi[[2]], ls_ndvi[[3]], ls_ndvi[[3]]), 
-                 k = list("a)", "b)", "c)"), 
+                 k = list("a)", "b)", "c)"), fun = list("median", "median", "median"),
                  l = list("terra|aqua", "terra|gimms", "aqua|gimms")) %do% {
           
   p <- visIOA(i, j, cores = 3, crs = "+init=epsg:4326", xlab = "", ylab = "",
               sp.layout = list("sp.text", loc = c(37.04, -3.35), 
                                txt = k, font = 2, cex = 1.1), 
-              filename = paste0(ch_dir_outdata, l, "_ioa_0312.tif"))
+              fun = fun,
+              filename = paste0(ch_dir_outdata, l, "_ioa_", fun, "_0312.tif"))
 
-  p <- p + as.layer(p_dem)
-  p <- envinmrRasterPlot(p)
-  
-  return(p)
-}
-
-
-  p <- visMannKendall(rst = rst_ndvi, xlab = "", ylab = "",
-                      p_value = .05, crs = "+init=epsg:4326",
-                      filename = paste0(ch_dir_outdata, i, "_mk05_0312.tif"), 
-                      format = "GTiff", overwrite = TRUE, 
-                      sp.layout = list("sp.text", loc = c(37.04, -3.35), 
-                                       txt = txt, font = 2, cex = 1.1))
-  
   p <- p + as.layer(p_dem)
   p <- envinmrRasterPlot(p)
   
@@ -74,90 +61,76 @@ p_ioa <- foreach(i = list(ls_ndvi[[1]], ls_ndvi[[1]], ls_ndvi[[2]]),
 }
 
 ## statistics
-fls_mk001 <- list.files(ch_dir_outdata, pattern = "mk05_0312.tif$", 
+fls_ioa <- list.files(ch_dir_outdata, pattern = "ioa_median_0312.tif$", 
                         full.names = TRUE)
-rst_mk001 <- lapply(fls_mk001, raster)
+rst_ioa <- lapply(fls_ioa, raster)
 
-# terra
-val_mk001_terra <- rst_mk001[[2]][]
-val_mk001_terra_abs <- sum(!is.na(rst_mk001[[2]][]))
-val_mk001_terra_rel <- val_mk001_terra_abs / ncell(rst_mk001[[2]])
-val_mk001_terra_abs_pos <- sum(val_mk001_terra > 0, na.rm = TRUE)
-val_mk001_terra_rel_pos <- val_mk001_terra_abs_pos / val_mk001_terra_abs
-val_mk001_terra_abs_neg <- sum(val_mk001_terra < 0, na.rm = TRUE)
-val_mk001_terra_rel_neg <- val_mk001_terra_abs_neg / val_mk001_terra_abs
+# terra vs. aqua
+val_ioa_modis <- rst_ioa[[2]][]
+val_ioa_modis_mu <- mean(val_ioa_modis)
+val_ioa_modis_quan <- quantile(val_ioa_modis, probs = seq(.25, .75, .25))
 
-# aqua
-val_mk001_aqua <- rst_mk001[[3]][]
-val_mk001_aqua_abs <- sum(!is.na(rst_mk001[[3]][]))
-val_mk001_aqua_rel <- val_mk001_aqua_abs / ncell(rst_mk001[[3]])
-val_mk001_aqua_abs_pos <- sum(val_mk001_aqua > 0, na.rm = TRUE)
-val_mk001_aqua_rel_pos <- val_mk001_aqua_abs_pos / val_mk001_aqua_abs
-val_mk001_aqua_abs_neg <- sum(val_mk001_aqua < 0, na.rm = TRUE)
-val_mk001_aqua_rel_neg <- val_mk001_aqua_abs_neg / val_mk001_aqua_abs
+# terra vs. gimms
+val_ioa_terra_gimms <- rst_ioa[[3]][]
+val_ioa_terra_gimms_mu <- mean(val_ioa_terra_gimms)
+val_ioa_terra_gimms_quan <- quantile(val_ioa_terra_gimms, 
+                                     probs = seq(.25, .75, .25))
 
-# mean difference
-val_mk001 <- cbind(val_mk001_terra, val_mk001_aqua)
-val_mk001_cc <- val_mk001[complete.cases(val_mk001), ]
-mean(val_mk001_cc[, 1] - val_mk001_cc[, 2])
-
-
-# gimms
-val_mk001_gimms <- rst_mk001[[1]][]
-val_mk001_gimms_abs <- sum(!is.na(rst_mk001[[1]][]))
-val_mk001_gimms_rel <- val_mk001_gimms_abs / ncell(rst_mk001[[1]])
-val_mk001_gimms_abs_pos <- sum(val_mk001_gimms > 0, na.rm = TRUE)
-val_mk001_gimms_rel_pos <- val_mk001_gimms_abs_pos / val_mk001_gimms_abs
-val_mk001_gimms_abs_neg <- sum(val_mk001_gimms < 0, na.rm = TRUE)
-val_mk001_gimms_rel_neg <- val_mk001_gimms_abs_neg / val_mk001_gimms_abs
+# aqua vs. gimms
+val_ioa_aqua_gimms <- rst_ioa[[1]][]
+val_ioa_aqua_gimms_mu <- mean(val_ioa_aqua_gimms)
+val_ioa_aqua_gimms_quan <- quantile(val_ioa_aqua_gimms, 
+                                     probs = seq(.25, .75, .25))
 
 ## trend values
-fls_mk <- list.files(ch_dir_outdata, pattern = paste0("mk001.*.tif$"), 
+fls_ioa <- list.files(ch_dir_outdata, pattern = paste0("ioa_median_0312.tif$"), 
                      full.names = TRUE)
-val_mk <- lapply(fls_mk, function(i) {
-  rst_mk <- raster(i)
-  values(rst_mk)
+val_ioa <- lapply(fls_ioa, function(i) {
+  rst_ioa <- raster(i)
+  values(rst_ioa)
 })
 
 ## density plot
 cols <- c("black", "black", "grey50")
 ltys <- c("longdash", "solid", "solid")
-names(ltys) <- names(cols) <- c("GIMMS", "Terra", "Aqua")
+names(ltys) <- names(cols) <- c("Aqua|GIMMS", "Terra|Aqua", "Terra|GIMMS")
 
 p_dens <- ggplot() + 
-  geom_line(aes(x = val_mk[[2]], y = ..count../sum(..count..), 
-                linetype = "Terra", colour = "Terra"), 
+  geom_line(aes(x = val_ioa[[2]], y = ..count../sum(..count..), 
+                linetype = "Terra|Aqua", colour = "Terra|Aqua"), 
             stat = "density", lwd = .8) + 
-  geom_line(aes(x = val_mk[[3]], y = ..count../sum(..count..), 
-                linetype = "Aqua", colour = "Aqua"), 
+  geom_line(aes(x = val_ioa[[3]], y = ..count../sum(..count..), 
+                linetype = "Terra|GIMMS", colour = "Terra|GIMMS"), 
             stat = "density", lwd = .8) + 
-  geom_line(aes(x = val_mk[[1]], y = ..count../sum(..count..), 
-                linetype = "GIMMS", colour = "GIMMS"), 
+  geom_line(aes(x = val_ioa[[1]], y = ..count../sum(..count..), 
+                linetype = "Aqua|GIMMS", colour = "Aqua|GIMMS"), 
             stat = "density", lwd = .8) + 
-  geom_text(aes(x = -.675, y = .016), label = "d)", fontface = "bold", size = 6) + 
-  scale_linetype_manual("", breaks = c("Terra", "Aqua", "GIMMS"), values = ltys) + 
-  scale_colour_manual("", breaks = c("Terra", "Aqua", "GIMMS"), values = cols) +
-  labs(x = expression(atop("", "Kendall's " * tau)), y = "Density\n") + 
+  geom_text(aes(x = .25, y = .008), label = "d)", fontface = "bold", size = 6) + 
+  scale_linetype_manual("", breaks = c("Terra|Aqua", "Terra|GIMMS", "Aqua|GIMMS"), values = ltys) + 
+  scale_colour_manual("", breaks = c("Terra|Aqua", "Terra|GIMMS", "Aqua|GIMMS"), values = cols) +
+  labs(x = "\nIOA", y = "Density\n") + 
   theme_bw() + 
   theme(text = element_text(size = 13), 
         panel.grid = element_blank(),
-        legend.key.size = unit(1, "cm"), 
+        legend.margin = unit(0, "mm"),
+        legend.key.size = unit(.9, "cm"), 
         legend.key = element_rect(colour = "transparent"),
+        legend.background = element_rect(colour = "transparent"),
         legend.text = element_text(size = 11),
-        legend.position = c(.8, .65), legend.justification = c("center", "center"), 
+        legend.position = c(.35, .7), legend.justification = c("center", "center"), 
         plot.margin = unit(rep(0, 4), units = "mm"), 
         panel.border = element_rect(colour = "black"))
 
 
 ## combination final figure
-p_mk_comb <- latticeCombineGrid(p_mk, layout = c(2, 2))
+p_ioa_comb <- latticeCombineGrid(p_ioa, layout = c(2, 2))
 
-ch_fls_fig <- paste0(ch_dir_outdata, "fig01__mannkendall05.png")
+ch_fls_fig <- paste0(ch_dir_outdata, "fig02__ioa.png")
 png(ch_fls_fig, width = 24, height = 26, units = "cm", 
     res = 300, pointsize = 15)
 plot.new()
 
-print(p_mk_comb, newpage = FALSE)
+print(p_ioa_comb, newpage = FALSE)
 
 vp_dens <- viewport(x = .52, y = 0.15, width = .4575, height = .3, 
                     just = c("left", "bottom"))
--- R/visIOA.R
+++ R/visIOA.R
@@ -1,7 +1,8 @@
 visIOA <- function(x, y, 
-                   cores = 1, 
+                   cores = 1,
+                   fun = c("mean", "median", "max"),
                    sp.layout = NULL,
-                   at = seq(0, 1, .1),
+                   at = seq(.2, 1, .05),
                    col.regions = NULL,
                    alpha.regions = 1,
                    crs = NULL, 
@@ -12,50 +13,62 @@ visIOA <- function(x, y,
   lib <- c("Rsenal", "RColorBrewer", "reshape2", "plyr")
   jnk <- sapply(lib, function(x) library(x, character.only = TRUE))
   
-  ## resample data if x and y have different spatial resolution
-  if (any(res(x) != res(y))) {
-    
-    # initialize parallel backend
-    library(doParallel)
-    cl <- makeCluster(cores)
-    registerDoParallel(cl)
-    
-    # template used for value extraction
-    spy_template <- rasterToPolygons(y[[1]])
-    
-    # value extraction
-    ls_vals <- foreach(i = 1:nlayers(x), 
-                       .packages = c("raster", "rgdal")) %dopar% {
-      val <- extract(x[[i]], spy_template)
-      val_stats <- lapply(val, function(j) {
-        mn <- mean(j, na.rm = TRUE)
-        qn <- quantile(j, probs = c(.1, .5, .9), na.rm = TRUE)
-        matrix(c(mn, qn), ncol = 4, byrow = TRUE)
-      })
-      val_stats <- do.call("rbind", val_stats)
-      return(val_stats)
-    }
-    
-    # checkout parallel backend
-    stopCluster(cl)
-  }
-  
-  # Colors
-  if (is.null(col.regions))
-    col.regions <- colorRampPalette(brewer.pal(11, "BrBG"))
-  
+  ## if file already exists, skip processing
   if (file.exists(filename)) {
     cat("File found.\n")
     rst_ioa <- raster(filename)
   } else {
     cat("File not found.\n")
     
+    ## resample data if x and y have different spatial resolution
+    if (any(res(x) != res(y))) {
+      
+      # initialize parallel backend
+      library(doParallel)
+      cl <- makeCluster(cores)
+      registerDoParallel(cl)
+      
+      # template used for value extraction
+      spy_template <- rasterToPolygons(y[[1]])
+      
+      # function to be applied
+      fun <- fun[1]
+      
+      # value extraction
+      ls_vals <- 
+        foreach(i = 1:nlayers(x), .packages = c("raster", "rgdal")) %dopar% {
+          val <- extract(x[[i]], spy_template)
+          val_stats <- lapply(val, function(j) {
+            mn <- mean(j, na.rm = TRUE)
+            qn <- quantile(j, probs = c(.1, .5, .9), na.rm = TRUE)
+            mx <- max(j, na.rm = TRUE)
+            matrix(c(mn, qn), ncol = 5, byrow = TRUE)
+          })
+          val_stats <- do.call("rbind", val_stats)
+          return(val_stats)
+        }
+      
+      # function selection
+      int_id_col <- if (fun == "mean") 1L else if (fun == "median") 3L else 5L
+      mat_x <- do.call("cbind", lapply(ls_vals, function(i) {
+        i[, int_id_col]
+      }))
+      mat_x <- data.frame(cell = 1:nrow(mat_x), mat_x)
+      names(mat_x)[2:ncol(mat_x)] <- formatC(1:(ncol(mat_x)-1), width = 3, flag = 0)
+      mat_x <- melt(mat_x, id.vars = 1)
+      
+      # check out parallel backend
+      stopCluster(cl)
+    }
+    
     # index of association (ioa)
-    mat_x <- getValues(x)
-    mat_x <- data.frame(cell = 1:ncell(x), mat_x)
-    names(mat_x)[2:ncol(mat_x)] <- formatC(1:(ncol(mat_x)-1), width = 3, flag = 0)
-    mat_x <- melt(mat_x, id.vars = 1)
-
+    if (!exists("mat_x")) {
+      mat_x <- getValues(x)
+      mat_x <- data.frame(cell = 1:ncell(x), mat_x)
+      names(mat_x)[2:ncol(mat_x)] <- formatC(1:(ncol(mat_x)-1), width = 3, flag = 0)
+      mat_x <- melt(mat_x, id.vars = 1)
+    }
+    
     mat_y <- getValues(y)
     mat_y <- data.frame(cell = 1:ncell(y), mat_y)
     names(mat_y)[2:ncol(mat_y)] <- formatC(1:(ncol(mat_y)-1), width = 3, flag = 0)
@@ -64,10 +77,10 @@ visIOA <- function(x, y,
     mat_mrg <- data.frame(mat_x, value_y = mat_y[, 3])
     
     num_ioa <- ddply(mat_mrg, .(mat_mrg$cell), 
-                     summarise, ioa = round(ioa(value, value_y), 2))
+                     summarise, ioa = ioa(value, value_y))
     
     # insert values
-    rst_ioa <- x[[1]]
+    rst_ioa <- y[[1]]
     rst_ioa[] <- num_ioa[, 2]
     
     # output storage (optional)
@@ -82,12 +95,15 @@ visIOA <- function(x, y,
     rst_ioa <- trim(rst_ioa)
   }
   
-  # Plotting
-  p <- spplot(rst_ioa, scales = list(draw = TRUE), xlab = xlab, ylab = ylab, 
-              col.regions = col.regions, 
-              sp.layout = sp.layout, 
-              par.settings = list(fontsize = list(text = 15)), 
-              at = at, alpha.regions = alpha.regions)
+  ## colors
+  if (is.null(col.regions))
+    col.regions <- colorRampPalette(brewer.pal(3, "Reds"))
   
-  return(p)
+  ## visualize
+  spplot(rst_ioa, scales = list(draw = TRUE), xlab = xlab, ylab = ylab, 
+         col.regions = col.regions, 
+         sp.layout = sp.layout, 
+         par.settings = list(fontsize = list(text = 15)), 
+         at = at, alpha.regions = alpha.regions)
+  
 }
