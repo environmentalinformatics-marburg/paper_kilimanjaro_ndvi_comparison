12c29386d5bbae6dee06cae63c3bae4474d2f17c
fdetsch <florian.detsch@staff.uni-marburg.de>
2015-10-01 17:10:41 +0200

    Adjustmented function to new file naming conventions.

--- R/aggregateGimms.R
+++ R/aggregateGimms.R
@@ -1,14 +1,21 @@
 aggregateGimms <- function(files,
-                           start = 4,
-                           stop = 8, 
+                           start = 5,
+                           stop = 11, 
                            fun = max,
+                           format = "%Y%j",
                            ...) {
 
+  ## packages
   library(raster)
+  library(zoo)
   
+  ## monthly indexes
   dates <- substr(basename(files), start, stop)
+  dates <- as.Date(dates, format = format)
+  dates <- as.yearmon(dates)
   indices <- as.numeric(as.factor(dates))
   
+  ## monthly aggregation
   rst <- stack(files)
   rst_agg <- stackApply(rst, indices = indices, fun = fun, ...)
   
