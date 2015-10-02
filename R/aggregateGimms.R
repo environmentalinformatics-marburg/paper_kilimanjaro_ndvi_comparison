f32759a6390c77f67f41266065bd8003345a7965
dogbert <dogbert@pc19ui245>
2015-10-02 13:45:43 +0200

    Enabled multi-core functionality.

--- R/aggregateGimms.R
+++ R/aggregateGimms.R
@@ -1,23 +1,43 @@
 aggregateGimms <- function(files,
+                           files_out, 
                            start = 5,
                            stop = 11, 
                            fun = max,
+                           nodes = 1L,
                            format = "%Y%j",
                            ...) {
 
   ## packages
   library(raster)
-  library(zoo)
-  
+
+  ## parallelization
+  library(doParallel)
+  cl_agg <- cluster(nodes)
+  registerDoParallel(cl_agg)
+
   ## monthly indexes
   dates <- substr(basename(files), start, stop)
-  dates <- as.Date(dates, format = format)
-  dates <- as.yearmon(dates)
   indices <- as.numeric(as.factor(dates))
-  
+
   ## monthly aggregation
   rst <- stack(files)
-  rst_agg <- stackApply(rst, indices = indices, fun = fun, ...)
+  
+  rst_agg <- if (missing(files_out)) {
+    foreach(i = unique(indices), .packages = c("raster", "rgdal")) %dopar% {
+      rst_tmp <- rst[[which(indices == i)]]
+      overlay(rst_tmp, fun = fun)
+    }
+    
+  } else {
+    foreach(i = unique(indices), j = 1:length(unique(indices)), 
+            .packages = c("raster", "rgdal")) %dopar% {
+      rst_tmp <- rst[[which(indices == i)]]
+      overlay(rst_tmp, fun = fun, filename = file_out[j], ...)
+    }
+  }
+  
+  #   rst <- stack(files)
+  #   rst_agg <- stackApply(rst, indices = indices, fun = fun, ...)
   
   return(rst_agg)
 }
