f0d7455152d47c1e8c606eedd63f413debaae2f7
fdetsch <florian.detsch@staff.uni-marburg.de>
2015-11-09 15:15:46 +0100

    Adjusted look and updated data required for figure 1 and table 1.

--- R/fig01_mannkendall.R
+++ R/fig01_mannkendall.R
@@ -47,13 +47,15 @@ num_ymax <- ymax(rst_ndvi)
 st_year <- "2003"
 nd_year <- "2012"
 
-products <- c("GIMMS3g", 
-              "MOD13Q1.005", "MYD13Q1.005", 
-              "MOD13Q1.006", "MYD13Q1.006")
+products <- list("GIMMS3g", 
+                 "MOD13Q1.005", "MYD13Q1.005", 
+                 "MOD13Q1.006", "MYD13Q1.006")
 
-label <- c("b)", "c)", "d)", "e)", "f)")
-label <- sapply(1:length(label), function(i) paste(label[i], products[i]))
-label[1] <- "b) GIMMS NDVI3g"
+labels <- list(expression(bold("b) NDVI"["3g"])), 
+               expression(bold("c) NDVI"["Terra-C5"])), 
+               expression(bold("d) NDVI"["Aqua-C5"])), 
+               expression(bold("e) NDVI"["Terra-C6"])), 
+               expression(bold("f) NDVI"["Aqua-C6"])))
 
 ## breaks (works only when mann-kendall trend layers already exist)
 fls_mk <- list.files(ch_dir_outdata, pattern = "mk05_0312", 
@@ -66,7 +68,7 @@ lst_mk <- lapply(fls_mk, raster)
 # })
 
 ## create and visualize mann-kendall trend layers
-p_mk <- foreach(i = products, txt = label, .packages = lib) %dopar% {
+p_mk <- foreach(i = products, txt = labels) %do% {
   
   fls_ndvi <- list.files(paste0(ch_dir_extdata, i), recursive = TRUE,
                          pattern = "^DSN_.*.tif$", full.names = TRUE)
@@ -77,10 +79,10 @@ p_mk <- foreach(i = products, txt = label, .packages = lib) %dopar% {
   fls_ndvi <- fls_ndvi[st:nd]
   rst_ndvi <- stack(fls_ndvi)
   
-  p <- visMannKendall(rst = rst_ndvi, xlab = "", ylab = "",
+  p <- visMannKendall(rst = rst_ndvi, dem = rst_dem, xlab = "", ylab = "",
                       p_value = .001, crs = "+init=epsg:4326",
-                      filename = paste0(ch_dir_outdata, i, "_mk001_0312.tif"), 
-                      at = seq(-.875, .875, .0875), 
+                      filename = paste0(ch_dir_outdata, i, "_mk_0312.tif"), 
+                      at = seq(-.875, .875, .0875), cores = 3L, 
                       format = "GTiff", overwrite = TRUE, 
                       sp.layout = list("sp.text", loc = c(37.04, -3.35), 
                                        txt = txt, font = 2, cex = .6, adj = c(.1, 1)), 
@@ -107,7 +109,7 @@ rst_kili <- raster(osm_kili)
 
 p_bing <- spplot(rst_kili[[1]], col.regions = NA, colorkey = FALSE, 
                  sp.layout = list(rgb2spLayout(rst_kili), 
-                                  list("sp.text", loc = c(37.04, -3.35), 
+                                  list("sp.text", loc = c(37.02, -3.35), 
                                        txt = "a)", font = 2, cex = .6, adj = c(.1, 1))),
                  xlim = c(num_xmin, num_xmax), 
                  ylim = c(num_ymin, num_ymax), 
@@ -199,14 +201,20 @@ dev.off()
 ## statistics
 ################################################################################
 
-## statistics
-fls_mk05 <- list.files(ch_dir_outdata, pattern = "mk05_0312.tif$", 
+# ## new product labels
+# labels <- list(expression("NDVI"["3g"]),    
+#                expression("NDVI"["Terra-C5"]), expression("NDVI"["Aqua-C5"]), 
+#                expression("NDVI"["Terra-C6"]), expression("NDVI"["Aqua-C6"]))
+
+
+## statistics (p < 0.001)
+fls_mk001 <- list.files(ch_dir_outdata, pattern = "0312_tau001.tif$", 
                        full.names = TRUE)[c(1, 2, 4, 3, 5)]
-rst_mk05 <- lapply(fls_mk05, raster)
+rst_mk001 <- lapply(fls_mk001, raster)
 
 # trend differences
-df_mk_stats <- foreach(i = rst_mk05, .combine = "rbind") %do% mkStats(i)
-df_mk_stats <- cbind(product = products, df_mk_stats)
+df_mk_stats <- foreach(i = rst_mk001, .combine = "rbind") %do% mkStats(i)
+df_mk_stats <- cbind(product = unlist(labels), df_mk_stats)
 
 # reformat
 df_mk_stats[, 1] <- as.character(df_mk_stats[, 1])
@@ -217,24 +225,42 @@ rownames(df_mk_stats)[1] <- "GIMMS NDVI3g"
 
 stargazer(df_mk_stats, summary = FALSE)
 
+# ## statistics (p < 0.05)
+# fls_mk05 <- list.files(ch_dir_outdata, pattern = "0312_tau05.tif$", 
+#                        full.names = TRUE)[c(1, 2, 4, 3, 5)]
+# rst_mk05 <- lapply(fls_mk05, raster)
+# 
+# # trend differences
+# df_mk_stats <- foreach(i = rst_mk05, .combine = "rbind") %do% mkStats(i)
+# df_mk_stats <- cbind(product = products, df_mk_stats)
+# 
+# # reformat
+# df_mk_stats[, 1] <- as.character(df_mk_stats[, 1])
+# rownames(df_mk_stats) <- df_mk_stats[, 1]
+# df_mk_stats <- df_mk_stats[, -c(1, 4, 6)]
+# names(df_mk_stats) <- c("Trend pixels", "Trends (%)", "Greening (%)", "Browning (%)")
+# rownames(df_mk_stats)[1] <- "GIMMS NDVI3g"
+# 
+# stargazer(df_mk_stats, summary = FALSE)
+
 # mean difference
 ## calculate ioa
-dat_meandiff <- foreach(i = 1:length(rst_mk05), .combine = "rbind") %do% {
+dat_meandiff <- foreach(i = 2:length(rst_mk001), .combine = "rbind") %do% {
   
-  foreach(j = 1:length(rst_mk05), .combine = "rbind") %do% {
+  foreach(j = 2:length(rst_mk001), .combine = "rbind") %do% {
     
     # if stacks are identical, ioa equals 1        
     if (i == j) {
       val_meandiff <- 0
     } else {
       
-      rst1 <- rst_mk05[[i]]
+      rst1 <- rst_mk001[[i]]
       
       # resample modis
       if (i == 1 & j != 1) {
-        rst2 <- resample(rst_mk05[[j]], rst1)
+        rst2 <- resample(rst_mk001[[j]], rst1)
       } else {
-        rst2 <- rst_mk05[[j]]
+        rst2 <- rst_mk001[[j]]
         
         if (i != 1 & j == 1) 
           rst1 <- resample(rst1, rst2)
@@ -254,23 +280,23 @@ dat_meandiff <- foreach(i = 1:length(rst_mk05), .combine = "rbind") %do% {
 }
 save(dat_meandiff, file = "data/meandiff.RData")
 
+products <- products[2:5]
 mat_meandiff <- matrix(ncol = length(products), nrow = length(products))
 rst_meandiff <- raster(ncols = length(products), nrows = length(products), 
-                       xmn = 0, xmx = 5, ymn = 0, ymx = 5)
+                       xmn = 0, xmx = 4, ymn = 0, ymx = 4)
 for (i in 1:length(products)) {
   sub <- subset(dat_meandiff, ref1 == rev(products)[i])
   mat_meandiff[i, ] <- sub$md
 }
 
-rst_meandiff <- raster(mat_meandiff, xmn = 0, xmx = 5, ymn = 0, ymx = 5)
-rst_meandiff[seq(5, ncell(rst_meandiff), 4)] <- NA
+rst_meandiff <- raster(mat_meandiff, xmn = 0, xmx = 4, ymn = 0, ymx = 4)
+rst_meandiff[seq(4, ncell(rst_meandiff)-1, 3)] <- NA
 
-txt <- c(expression("NDVI"['3g']), 
-         expression("NDVI"['Terra-C5']), expression("NDVI"['Aqua-C5']), 
+txt <- c(expression("NDVI"['Terra-C5']), expression("NDVI"['Aqua-C5']), 
          expression("NDVI"['Terra-C6']), expression("NDVI"['Aqua-C6']))
 cols <- colorRampPalette(brewer.pal(11, "RdBu"))
 p_meandiff <- 
-  spplot(rst_meandiff, col.regions = cols(100), at = seq(-.1, .1, .02), 
+  spplot(rst_meandiff, col.regions = cols(100), at = seq(-.05, .05, .01), 
          scales = list(draw = TRUE, at = seq(.5, 4.5, 1), labels = txt, 
                        cex = .8, x = list(rot = 45)), 
          colorkey = list(space = "top", labels = list(cex = .8), width = .7)) + 
@@ -278,14 +304,33 @@ p_meandiff <-
 
 png(paste0(ch_dir_outdata, "figure02.png"), width = 10.5, height = 12, 
     units = "cm", res = 500)
-print(p_meandiff)
+# main figure
+grid.newpage()
+vp0 <- viewport(x = 0, y = 0, width = 1, height = .9, 
+                just = c("left", "bottom"), name = "vp_figure")
+pushViewport(vp0)
+print(p_meandiff, newpage = FALSE)
+
+# key caption
+downViewport(trellis.vpname("figure"))
+grid.text(expression("MD"[tau]), x = 0.5, y = 1.3, just = c("centre", "bottom"), 
+          gp = gpar(font = 2, cex = .9))
 dev.off()
 
 tiff(paste0(ch_dir_outdata, "figure02.tiff"), width = 10.5, height = 12, 
      units = "cm", res = 500, compression = "lzw")
-print(p_meandiff)
-dev.off()
+# main figure
+grid.newpage()
+vp0 <- viewport(x = 0, y = 0, width = 1, height = .9, 
+                just = c("left", "bottom"), name = "vp_figure")
+pushViewport(vp0)
+print(p_meandiff, newpage = FALSE)
 
+# key caption
+downViewport(trellis.vpname("figure"))
+grid.text(expression("MD"[tau]), x = 0.5, y = 1.3, just = c("centre", "bottom"), 
+          gp = gpar(font = 2, cex = .9))
+dev.off()
 
 ## deregister parallel backend
 stopCluster(cl)
--- R/visMannKendall.R
+++ R/visMannKendall.R
@@ -1,4 +1,4 @@
-visMannKendall <- function(rst, 
+visMannKendall <- function(rst, dem, max_ele,
                            p_value = NULL, 
                            sp.layout = NULL,
                            at = seq(-1, 1, .2),
@@ -8,49 +8,120 @@ visMannKendall <- function(rst,
                            crs = NULL, 
                            xlab = "x", ylab = "y", scales = list(draw = TRUE),
                            xlim = bbox(rst)[1, ], ylim = bbox(rst)[2, ],
-                           filename = "", rewrite = FALSE, ...) {
+                           filename = "", rewrite = FALSE, cores = 1L, ...) {
   
-  lib <- c("raster", "Kendall", "rasterVis", "RColorBrewer")
-  jnk <- sapply(lib, function(x) library(x, character.only = TRUE))
+  ## parallelize
+  if (cores > 1L) {
+    cl <- parallel::makeCluster(cores)
+    doParallel::registerDoParallel(cl)
+  }
+    
+  # output files
+  file_tau <- paste0(dirname(filename), Orcs::pureBasename(filename, TRUE), "_tau.tif")
+  file_p <- paste0(dirname(filename), Orcs::pureBasename(filename, TRUE), "_p.tif")
   
-  # Colors
-  if (is.null(col.regions))
-    col.regions <- colorRampPalette(brewer.pal(11, "BrBG"))
-  
-  if (file.exists(filename) & !rewrite) {
+  if (file.exists(file_tau) & file.exists(file_p) & !rewrite) {
     cat("Files found, working with existing files.\n")
-    ndvi.mk <- raster(filename)
+    ndvi.mk <- raster::stack(file_tau, file_p)
   } else {
     cat("Files in, start processing.\n")
     
     # Mann-Kendall tau
-    if (is.null(p_value)) {
-      ndvi.mk <- overlay(rst, fun = function(x) {
-        Rsenal::significantTau(x, p = 1, prewhitening = TRUE)
-      }, filename = filename, overwrite = TRUE, ...)
-      
-    } else {
-      # Mann-Kendall tau of significant pixels only
-      ndvi.mk <- overlay(rst, fun = function(x) {
-        Rsenal::significantTau(x, p = p_value, prewhitening = TRUE)
-      }, filename = filename, overwrite = TRUE, ...)
+    mat <- raster::as.matrix(rst)
+    mat_mk <- foreach::foreach(i = 1:nrow(mat), .packages = "gimms", 
+                               .combine = "rbind") %dopar% {
+                                 gimms::significantTau(mat[i, ], df = TRUE, 
+                                                       prewhitening = TRUE, 
+                                                       conf.intervals = FALSE)
+                               }
+    
+    if (nrow(mat_mk) != raster::ncell(rst))
+      stop("Input raster and output matrix are of different size!\n")
+    
+    # raster templates
+    rst_tau <- rst_p <- rst[[1]]
+    rst_tau[] <- rst_p[] <- NA
+    
+    # insert values
+    rst_tau[] <- mat_mk[, 1]
+    rst_p[] <- mat_mk[, 2]
+    
+    # reject values above user-defined elevation level
+    if (!missing(dem) & !missing(max_ele)) {
+      rst_tau <- overlay(rst_tau, dem, fun = function(x, y) {
+        x[y[] > max_ele] <- NA
+        return(x)
+      })
+      rst_p <- overlay(rst_p, dem, fun = function(x, y) {
+        x[y[] > max_ele] <- NA
+        return(x)
+      })    
     }
+
+    rst_out <- raster::stack(rst_tau, rst_p)
+    
+    # write to file
+    lst_out <- foreach::foreach(i = 1:2, j = c(file_tau, file_p)) %do% {
+      raster::writeRaster(rst_out[[i]], filename = j, ...)
+    }
+    
+    ndvi.mk <- raster::stack(lst_out)
+  }
+
+  
+  # reject non-significant pixels (optional)    
+  if (!is.null(p_value)) {
+    file_sign <- paste0(dirname(filename), Orcs::pureBasename(filename, TRUE), 
+                        "_tau", substr(p_value, 3, nchar(p_value)))
+    ndvi.mk <- raster::overlay(ndvi.mk[[1]], ndvi.mk[[2]], 
+                               fun = function(x, y) {
+                                 x[y[] >= p_value] <- NA
+                                 return(x)
+                               }, filename = file_sign, 
+                               format = "GTiff", overwrite = TRUE)
   }
   
-  ## status message
-  cat("minValue:", minValue(ndvi.mk), "maxValue:", maxValue(ndvi.mk), "\n")
-  
+  # reproject (optional)
   if (!is.null(crs)) {
-    ndvi.mk <- projectRaster(ndvi.mk, crs = crs, method = "ngb")
-    ndvi.mk <- trim(ndvi.mk)
+    ndvi.mk <- raster::projectRaster(ndvi.mk, crs = crs, method = "ngb")
+    if (!all(is.na(ndvi.mk[])))
+      ndvi.mk <- raster::trim(ndvi.mk)
   }
   
-  # Plotting
-  spplot(ndvi.mk, scales = scales, xlab = xlab, ylab = ylab, 
-         col.regions = col.regions, 
-         sp.layout = sp.layout, xlim = xlim, ylim = ylim,
-         par.settings = list(fontsize = list(text = 15)), 
-         at = at, alpha.regions = alpha.regions, 
-         colorkey = list(labels = list(cex = keycex)))
+  ## deregister parallel backend
+  if (cores > 1L) {
+    parallel::stopCluster(cl)
+  }
+  
+  ## create figure
+  
+  # colors
+  if (is.null(col.regions))
+    col.regions <- colorRampPalette(RColorBrewer::brewer.pal(11, "BrBG"))
+  
+  # only missing values
+  if (all(is.na(ndvi.mk[]))) {
+    
+    ndvi.mk[] <- 0
+    sp::spplot(ndvi.mk, col.regions = "transparent", scales = scales, 
+               colorkey = FALSE, xlab = xlab, ylab = ylab, 
+               xlim = xlim, ylim = ylim) + 
+      latticeExtra::layer(sp::sp.polygons(raster::rasterToPolygons(ndvi.mk), 
+                                          lty = "dotted", col = "grey40"), 
+                          data = list(ndvi.mk = ndvi.mk)) + 
+      latticeExtra::layer(sp::sp.text(loc = c(37.04, -3.35), 
+                                      txt = expression(bold("b) NDVI"["3g"])), 
+                                      font = 2, cex = .6, adj = c(.1, 1)))
+    
+  # valid values  
+  } else {
+    
+    sp::spplot(ndvi.mk, scales = scales, xlab = xlab, ylab = ylab, 
+               col.regions = col.regions, 
+               sp.layout = sp.layout, xlim = xlim, ylim = ylim,
+               par.settings = list(fontsize = list(text = 15)), 
+               at = at, alpha.regions = alpha.regions, 
+               colorkey = list(labels = list(cex = keycex)))
+  }
   
 }
